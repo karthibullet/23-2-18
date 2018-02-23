f0ae2d81c9e056930a7786e4089dbf6b534c9f2b
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-23 15:41:00 +0530

    52.c

---
+++ 52.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("enter the value");
+    scanf("%d",&x);
+    if(N<=10)
+    {
+        switch(0)
+        {
+        case 1:"one";
+        break;
+        case 2:"two";
+        break;
+        case 3:"three";
+        break;
+        case 4:"four";
+        break;
+        case 5:"five";
+        break;
+        }
+        }
+        }
